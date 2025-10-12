553cc12883b2a298ac06e8e8f4ccb0413b1c311c
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-12 17:07:11 +0100

    passing props for stat card and first jsx

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import CardPopularProducts from "./CardPopularProducts";
 import CardSalesSummary from "./CardSalesSummary";
 import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardExpenseSummary from "./CardExpenseSummary";
+import StatCard from "./StatCard";
+import { Package, TrendingDown, TrendingUp } from "lucide-react";
 
 const Dashboard = () => {
   return (
@@ -13,7 +15,25 @@ const Dashboard = () => {
       <CardSalesSummary />
       <CardPurchaseSummary />
       <CardExpenseSummary />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500 " />
+      <StatCard
+        title="Customer & Expenses"
+        primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2025"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "175.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses",
+            amount: "10.00",
+            changePercentage: -65,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500 " />
       <div className="md:row-span-1 xl:row-span-2  bg-gray-500" />
     </div>
