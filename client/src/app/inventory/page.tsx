8b9319820300c5349a55eb04fbbc9247a8be841e
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-19 17:01:42 +0100

    create inventory page with data grid

---
+++ client/src/app/inventory/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useGetProductsQuery } from "@/state/api";
+import Header from "@/app/(components)/Header";
+import { DataGrid } from "@mui/x-data-grid";
+import { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  {
+    field: "productId",
+    headerName: "ID",
+    width: 90,
+  },
+  {
+    field: "name",
+    headerName: "Product Name",
+    width: 200,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? `${row.rating}` : "N/A"),
+  },
+  {
+    field: "stockQuantity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
+];
+
+const Inventory = () => {
+  const { data: products, isLoading, isError } = useGetProductsQuery();
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !products) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col">
+      <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Inventory;
