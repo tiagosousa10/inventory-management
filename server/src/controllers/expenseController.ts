d799b95c828ab113c950511dc9e4ca659d992f1a
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 19:42:08 +0100

    get user expenses

---
+++ server/src/controllers/expenseController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getExpenses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const expensesByCategoryRaw = await prisma.expenseByCategory.findMany({
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    const expenseByCategorySummary = expensesByCategoryRaw.map((item) => ({
+      ...item,
+      amount: item.amount.toString(),
+    }));
+    res.json(expenseByCategorySummary);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting expenses" });
+  }
+};
