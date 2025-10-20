d022c7ac052d4f3df008cff4c94ab1ae43c9cd78
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 11:22:48 +0100

    get users functionality and products modal updated

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -55,7 +55,8 @@ const CreateProductModal = ({
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
         <Header name="Create New Product" />
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="mt-5">
+          {/* product name */}
           <label htmlFor="productName" className={labelCssStyles}>
             Product Name
           </label>
@@ -68,6 +69,60 @@ const CreateProductModal = ({
             className={inputCssStyles}
             required
           />
+          {/* price */}
+          <label htmlFor="productPrice" className={labelCssStyles}>
+            Price
+          </label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            onChange={handleChange}
+            value={formData.price}
+            className={inputCssStyles}
+            required
+          />
+          {/* stockQuantity */}
+          <label htmlFor="stockQuantity" className={labelCssStyles}>
+            Stock Quantity
+          </label>
+          <input
+            type="number"
+            name="stockQuantity"
+            placeholder="Stock Quantity"
+            onChange={handleChange}
+            value={formData.stockQuantity}
+            className={inputCssStyles}
+            required
+          />
+          {/* rating */}
+          <label htmlFor="rating" className={labelCssStyles}>
+            Stock Quantity
+          </label>
+          <input
+            type="number"
+            name="rating"
+            placeholder="Rating"
+            onChange={handleChange}
+            value={formData.rating}
+            className={inputCssStyles}
+            required
+          />
+
+          {/* create actions */}
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Create
+          </button>
+          <button
+            type="button"
+            onClick={onClose}
+            className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700"
+          >
+            Cancel
+          </button>
         </form>
       </div>
     </div>
