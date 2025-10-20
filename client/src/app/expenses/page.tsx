b31c864f3d4aef4906482d96fa0d5672e3584589
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 21:39:22 +0100

    expenses page update with pie chart

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -1,22 +1,75 @@
 "use client";
 
-import { useGetExpensesByCategoryQuery } from "@/state/api";
+import {
+  ExpenseByCategorySummary,
+  useGetExpensesByCategoryQuery,
+} from "@/state/api";
 import { useMemo, useState } from "react";
 import Header from "@/app/(components)/Header";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+type AggregatedDataItem = {
+  name: string;
+  color?: string;
+  amount: number;
+};
+
+type AggregatedData = {
+  [category: string]: AggregatedDataItem;
+};
 
 const Expenses = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const {
     data: expensesData,
     isLoading,
     isError,
   } = useGetExpensesByCategoryQuery();
-
   const expenses = useMemo(() => expensesData ?? [], [expensesData]);
+
+  const parseDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
+
+  const aggregatedData: AggregatedDataItem[] = useMemo(() => {
+    const filtered: AggregatedData = expenses
+      .filter((data: ExpenseByCategorySummary) => {
+        const matchesCategory =
+          selectedCategory === "All" || data.category === selectedCategory;
+        const dataDate = parseDate(data.date);
+        const matchesDate =
+          !startDate ||
+          !endDate ||
+          (dataDate >= startDate && dataDate <= endDate);
+        return matchesCategory && matchesDate;
+      })
+      .reduce((acc: AggregatedData, data: ExpenseByCategorySummary) => {
+        const amount = parseInt(data.amount);
+        if (!acc[data.category]) {
+          acc[data.category] = { name: data.category, amount: 0 };
+          acc[data.category].color = `#${Math.floor(
+            Math.random() * 16777215
+          ).toString(16)}`;
+          acc[data.category].amount += amount;
+        }
+        return acc;
+      }, {});
+
+    return Object.values(filtered);
+  }, [expenses, selectedCategory, startDate, endDate]);
+
   const classNames = {
     label: "block text-sm font-medium text-gray-700",
     selectInput:
@@ -37,39 +90,96 @@ const Expenses = () => {
 
   return (
     <div>
-      {/* header */}
-      <div className="mt-5">
+      {/* HEADER */}
+      <div className="mb-5">
         <Header name="Expenses" />
         <p className="text-sm text-gray-500">
-          A visual representation of your expenses.
+          A visual representation of expenses over time.
         </p>
       </div>
 
-      {/* filters */}
+      {/* FILTERS */}
       <div className="flex flex-col md:flex-row justify-between gap-4">
         <div className="w-full md:w-1/3 bg-white shadow rounded-lg p-6">
           <h3 className="text-lg font-semibold mb-4">
             Filter by Category and Date
           </h3>
           <div className="space-y-4">
+            {/* CATEGORY */}
             <div>
               <label htmlFor="category" className={classNames.label}>
                 Category
               </label>
               <select
-                name="category"
                 id="category"
+                name="category"
                 className={classNames.selectInput}
-                defaultValue={"All"}
+                defaultValue="All"
                 onChange={(e) => setSelectedCategory(e.target.value)}
               >
-                <option value="">All</option>
-                <option value="">Office</option>
-                <option value="">Professional</option>
-                <option value="">Salaries</option>
+                <option>All</option>
+                <option>Office</option>
+                <option>Professional</option>
+                <option>Salaries</option>
               </select>
             </div>
+            {/* START DATE */}
+            <div>
+              <label htmlFor="start-date" className={classNames.label}>
+                Start Date
+              </label>
+              <input
+                type="date"
+                id="start-date"
+                name="start-date"
+                className={classNames.selectInput}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </div>
+            {/* END DATE */}
+            <div>
+              <label htmlFor="end-date" className={classNames.label}>
+                End Date
+              </label>
+              <input
+                type="date"
+                id="end-date"
+                name="end-date"
+                className={classNames.selectInput}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
           </div>
+        </div>
+        {/* PIE CHART */}
+        <div className="flex-grow bg-white shadow rounded-lg p-4 md:p-6">
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie
+                data={aggregatedData}
+                cx="50%"
+                cy="50%"
+                label
+                outerRadius={150}
+                fill="#8884d8"
+                dataKey="amount"
+                onMouseEnter={(_, index) => setActiveIndex(index)}
+              >
+                {aggregatedData.map(
+                  (entry: AggregatedDataItem, index: number) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={
+                        index === activeIndex ? "rgb(29, 78, 216)" : entry.color
+                      }
+                    />
+                  )
+                )}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
