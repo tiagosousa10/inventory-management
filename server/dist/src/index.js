dd862be4371796fdbd6d3a34050ae714a0ff8571
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 11:24:59 +0100

    create users routes

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -12,6 +12,7 @@ const morgan_1 = __importDefault(require("morgan"));
 /* ROUTE IMPORTS */
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -28,6 +29,7 @@ app.get("/hello", (req, res) => {
 });
 app.use("/dashboard", dashboardRoutes_1.default);
 app.use("/products", productRoutes_1.default);
+app.use("/users", userRoutes_1.default);
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -26,6 +27,7 @@ app.get("/hello", (req, res) => {
 });
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
